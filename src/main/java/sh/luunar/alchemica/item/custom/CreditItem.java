8722d2fd14803183a7f72ca8d39830fdbd7409fe
Kolbenik <niklas.miltz@gmail.com>
2025-11-20 20:16:19 +0100

    Enhance tooltip for CREDIT_LUNAR item with italic gray styling

--- src/main/java/sh/luunar/alchemica/item/custom/CreditItem.java
+++ src/main/java/sh/luunar/alchemica/item/custom/CreditItem.java
@@ -3,7 +3,11 @@ package sh.luunar.alchemica.item.custom;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import net.minecraft.util.Formatting;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import sh.luunar.alchemica.item.ModItems;
@@ -24,10 +28,9 @@ public class CreditItem extends Item {
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         if (stack.getItem().equals(ModItems.CREDIT_LUNAR)) {
             tooltip.add(Text.translatable("tooltip.credit_lunar"));
-            tooltip.add(Text.translatable("tooltip.credit_lunar.2"));
+            tooltip.add(Text.translatable("tooltip.credit_lunar.2").setStyle(Style.EMPTY.withFormatting(Formatting.ITALIC, Formatting.GRAY)));
         } else if (stack.getItem().equals(ModItems.CREDIT_KOLBENIK)) {
             tooltip.add(Text.translatable("tooltip.credit_kolbenik"));
         }
-
     }
 }
